b6c20ecd7e0e8bf1f96bb98b24018734b301cc0d
Ryutaitabashi <car.ryu.ryu.ryu@gmail.com>
2022-12-10 16:02:20 +0000

    [add]add openocd environment and LED blinker

--- standalone_app/xbee_spi_test_app/Core/Src/main.c
+++ standalone_app/xbee_spi_test_app/Core/Src/main.c
@@ -95,7 +95,10 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_GPIO_WritePin( GPIOA, GPIO_PIN_5, GPIO_PIN_SET );
+    HAL_Delay( 1000 );
+    HAL_GPIO_WritePin( GPIOA, GPIO_PIN_5, GPIO_PIN_RESET );
+    HAL_Delay( 1000 );
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
