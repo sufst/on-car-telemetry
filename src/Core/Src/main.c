9bfce848c2b33f4d81341c44a158a3a948397c73
Piotr Mikulowski <mikuspiotr123@gmail.com>
2023-02-09 21:49:54 +0000

    Xbee-stm32 Uart demo init

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -26,7 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -52,7 +52,49 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
+/*# 7- Retarget printf to UART (std library and toolchain dependent) #########*/
 
+#if defined(__GNUC__)
+int _write(int fd, char * ptr, int len)
+{
+  HAL_UART_Transmit(&huart4, (uint8_t *) ptr, len, HAL_MAX_DELAY);
+  return len;
+}
+#elif defined (__ICCARM__)
+#include "LowLevelIOInterface.h"
+size_t __write(int handle, const unsigned char * buffer, size_t size)
+{
+  HAL_UART_Transmit(&huart4, (uint8_t *) buffer, size, HAL_MAX_DELAY);
+  return size;
+}
+#elif defined (__CC_ARM)
+int fputc(int ch, FILE *f)
+{
+    HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    return ch;
+}
+#endif
+
+// OR:
+
+// Add syscalls.c with GCC
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  return ch;
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
--- src/SUSFT/Src/uart_thread.c
+++ src/SUSFT/Src/uart_thread.c
@@ -1,6 +1,9 @@
 #include "usart.h"
 #include "tx_api.h"
 #include "stdint.h"
+#include "xbee_platform.h"
+#include <stdio.h>
+#include "_atinter.h"
 /**
  * @brief Blink thread instance
  */
@@ -15,7 +18,9 @@ static TX_THREAD uart_thread;
  * function prototypes
  */
 static void uart_thread_entry(ULONG thread_input);
-
+xbee_dev_t my_xbee;
+xbee_serial_t *serport;
+char cmdstr[80];
 /**
  * @brief 		Creates the uart thread
  *
@@ -50,6 +55,43 @@ UINT uart_thread_create(TX_BYTE_POOL* stack_pool_ptr)
 }
 
 
+void setup_xbee(){
+  int status;
+    xbee_cmd_init_device( &my_xbee);
+    printf( "Waiting for driver to query the XBee device...\n");
+    do {
+        xbee_dev_tick( &my_xbee);
+        status = xbee_cmd_query_status( &my_xbee);
+    } while (status == -EBUSY);
+    if (status)
+    {
+        printf( "Error %d waiting for query to complete.\n", status);
+    }
+
+    // report on the settings
+    xbee_dev_dump_settings( &my_xbee, XBEE_DEV_DUMP_FLAG_DEFAULT);
+
+    printATCmds( &my_xbee);
+
+}
+
+void try_commands(){
+
+        while (xbee_readline( cmdstr, sizeof cmdstr) == -EAGAIN)
+        {
+            xbee_dev_tick( &my_xbee);
+        }
+
+        if (! strncmpi( cmdstr, "menu", 4))
+        {
+            printATCmds( &my_xbee);
+        }
+        else
+        {
+            process_command( &my_xbee, cmdstr);
+        }
+
+}
 /**
  * @brief       Blink thread entry function
  *
@@ -60,17 +102,43 @@ UINT uart_thread_create(TX_BYTE_POOL* stack_pool_ptr)
  */
 void uart_thread_entry(ULONG thread_input)
 {
-
+  setup_xbee();
 (void) thread_input;
-uint8_t data = 'A';
+uint8_t UART1_rxBuffer[12] = {0};
+uint8_t UART1_OK[2] = "OK";
+int a;
+HAL_StatusTypeDef ret;
+serport->huart = &huart4;
   /* Infinite loop */
   while(1)
   {
+  //char ch = 'a';
+  //ret = HAL_UART_Receive(&huart4, UART1_rxBuffer, 1, 0);
+  //ch = xbee_ser_getchar(serport); 
+  //xbee_ser_putchar(serport, ch);
 
-  HAL_UART_Transmit(&huart4, &data, 1, 100);
-  data++;
+  try_commands();
+  //while (xbee_readline( cmdstr, sizeof cmdstr) == -EAGAIN){}
+  
+  //xbee_ser_write(&huart4,UART1_OK,2);
+
+  //a = xbee_ser_read(serport, &UART1_rxBuffer, 5);
+  
+  //if(a == 5){
+  //  xbee_ser_write(serport, &UART1_rxBuffer, 5);
+  //  }
+
     /* Delay for 1000ms */
-  tx_thread_sleep(100);
+// tx_thread_sleep(100);
   }
 
 }
+
+#include "xbee_atcmd.h"         // for XBEE_FRAME_HANDLE_LOCAL_AT
+#include "xbee_device.h"
+const xbee_dispatch_table_entry_t xbee_frame_handlers[] =
+{
+    XBEE_FRAME_HANDLE_LOCAL_AT,
+    XBEE_FRAME_MODEM_STATUS_DEBUG,
+    XBEE_FRAME_TABLE_END
+};
