e853d5fa014d358ef620d1409a49f750211eacae
Piotr Mikulowski <mikuspiotr123@gmail.com>
2023-02-12 18:43:50 +0000

    Add publisher_context and publisher_init for can_publisher thread

--- src/SUFST/inc/can_publisher.h
+++ src/SUFST/inc/can_publisher.h
@@ -2,9 +2,21 @@
 #define CAN_PUBLISHER_H
 #include <stdint.h>
 #include <tx_api.h>
+#include "telemetry_protocol.h"
 
-extern TX_QUEUE queue;
-UINT queue_send_thread_create(TX_BYTE_POOL* stack_pool_ptr);
+#define TX_QUEUE_SIZE 10
+
+typedef struct {
+
+  TX_THREAD thread;
+
+  TX_QUEUE tx_queue;
+
+  rtcan_msg_t* tx_queue_mem[TX_QUEUE_SIZE];
+
+} publisher_context_t;
+
+UINT can_publisher_init(publisher_context_t* publisher_ptr, TX_BYTE_POOL* stack_pool_ptr);
 void queue_send_thread_entry(ULONG input);
 
 
