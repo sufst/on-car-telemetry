595f4ecdbb3c95cfe3ec19e71fa12aec55f7dbf3
Ryutaitabashi <car.ryu.ryu.ryu@gmail.com>
2023-03-07 19:47:58 +0000

    [add] added lookup table with random uint8_t*128

---
+++ src/SUFST/inc/Debug/testbench_can_data.h
@@ -0,0 +1,19 @@
+/***************************************************************************
+ * @file   testbench_can_data.h
+ * @author Ryuta Itabashi (@h2so4, ri1g22@soton.ac.uk)
+ * @date   2023-03-07
+ * @brief  Dummy data lookup table for simulating CAN bus communication
+ ***************************************************************************/
+
+#include <stdint.h>
+
+#include "config.h"
+
+#ifndef TESTBENCH_CAN_DATA_H
+#define TESTBENCH_CAN_DATA_H
+
+#if CAN_DEBUG_MODE == 1
+extern const uint8_t debug_lookup[1001];
+#endif
+
+#endif
