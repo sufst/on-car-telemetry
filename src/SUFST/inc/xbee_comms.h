5c5e52b7923089a596e5db3bda3c5405a7b64a54
Piotr Mikulowski <mikuspiotr123@gmail.com>
2023-03-23 19:21:05 +0000

    Resolve comments

--- src/SUFST/inc/xbee_comms.h
+++ src/SUFST/inc/xbee_comms.h
@@ -15,6 +15,10 @@ typedef struct {
 
   TX_QUEUE* rx_queue;
 
+  xbee_serial_t xbee_serial;
+
+  xbee_dev_t xbee_dev;
+
 } xbee_comms_context_t;
 
 UINT xbee_comms_init(xbee_comms_context_t* xbee_comms_ptr, TX_QUEUE* rx_queue, TX_BYTE_POOL* stack_pool_ptr);
--- src/SUFST/src/xbee_comms.c
+++ src/SUFST/src/xbee_comms.c
@@ -10,14 +10,12 @@
 #include "xbee/delivery_status.h"
 
 
-#define QUEUE_SEND_THREAD_PRIORITY             10
-#define QUEUE_SEND_THREAD_STACK_SIZE           1024
-#define QUEUE_SEND_THREAD_PREEMPTION_THRESHOLD 10                
+#define XBEE_COMMS_THREAD_PRIORITY             10
+#define XBEE_COMMS_THREAD_STACK_SIZE           1024
+#define XBEE_COMMS_THREAD_PREEMPTION_THRESHOLD 10                
 
-void xbee_comms_entry(ULONG input);
-xbee_serial_t xbee_serial;
-xbee_dev_t xbee_dev;
-int transmit_status_handler( xbee_dev_t *xbee, const void FAR *frame, uint16_t length, void FAR *context);
+static void xbee_comms_entry(ULONG input);
+static int transmit_status_handler( xbee_dev_t *xbee, const void FAR *frame, uint16_t length, void FAR *context);
 
 const xbee_dispatch_table_entry_t xbee_frame_handlers[] = {
   {XBEE_FRAME_TRANSMIT_STATUS, 0, transmit_status_handler, NULL}, //Fill in 0x8B Handler
@@ -30,7 +28,7 @@ int transparent_rx( const wpan_envelope_t FAR *envelope, void FAR *context)
 }
 
 // must be sorted by cluster ID
-const wpan_cluster_table_entry_t digi_data_clusters[] =
+static const wpan_cluster_table_entry_t digi_data_clusters[] =
 {
    // transparent serial goes here (cluster 0x0011)
    { DIGI_CLUST_SERIAL, transparent_rx, NULL,
@@ -44,9 +42,9 @@ const wpan_cluster_table_entry_t digi_data_clusters[] =
 
 /// Used to track ZDO transactions in order to match responses to requests
 /// (#ZDO_MATCH_DESC_RESP).
-wpan_ep_state_t zdo_ep_state = { 0 };
+static wpan_ep_state_t zdo_ep_state = { 0 };
 
-const wpan_endpoint_table_entry_t sample_endpoints[] = {
+static const wpan_endpoint_table_entry_t sample_endpoints[] = {
    ZDO_ENDPOINT(zdo_ep_state),
 
    // Endpoint/cluster for transparent serial and OTA command cluster
@@ -75,10 +73,11 @@ UINT xbee_comms_init(xbee_comms_context_t* xbee_comms_ptr, TX_QUEUE* rx_queue, T
     xbee_comms_ptr->rx_queue = rx_queue;
 
     VOID* thread_stack_ptr = NULL;
+    int xbee_status;
     //@todo Implement queue
     UINT tx_status = tx_byte_allocate(stack_pool_ptr,
                                 &thread_stack_ptr,
-                                QUEUE_SEND_THREAD_STACK_SIZE,
+                                XBEE_COMMS_THREAD_STACK_SIZE,
                                 TX_NO_WAIT);
 
     if (tx_status == TX_SUCCESS)
@@ -88,22 +87,25 @@ UINT xbee_comms_init(xbee_comms_context_t* xbee_comms_ptr, TX_QUEUE* rx_queue, T
                                xbee_comms_entry,
                                xbee_comms_ptr,
                                thread_stack_ptr,
-                               QUEUE_SEND_THREAD_STACK_SIZE,
-                               QUEUE_SEND_THREAD_PRIORITY,
-                               QUEUE_SEND_THREAD_PREEMPTION_THRESHOLD,
+                               XBEE_COMMS_THREAD_STACK_SIZE,
+                               XBEE_COMMS_THREAD_PRIORITY,
+                               XBEE_COMMS_THREAD_PREEMPTION_THRESHOLD,
                                TX_NO_TIME_SLICE,
                                TX_AUTO_START);
     }
 
-    /* Init xbee_serial_t struct */
-    xbee_serial.baudrate = 115200;
-    xbee_serial.huart = &huart4;
+    if(tx_status == TX_SUCCESS)
+    {
+        /* Init xbee_serial_t struct */
+        xbee_comms_ptr->xbee_serial.baudrate = 115200;
+        xbee_comms_ptr->xbee_serial.huart = &huart4;
 
-    int xbee_status = xbee_dev_init (&xbee_dev, &xbee_serial, NULL, NULL);
+        xbee_status = xbee_dev_init (&xbee_comms_ptr->xbee_dev, &xbee_comms_ptr->xbee_serial, NULL, NULL);
+    }
 
     if(xbee_status == 0)
     {
-        xbee_status = xbee_wpan_init(&xbee_dev, sample_endpoints);
+        xbee_status = xbee_wpan_init(&xbee_comms_ptr->xbee_dev, sample_endpoints);
     }
     
     if(xbee_status != 0)
@@ -115,16 +117,16 @@ UINT xbee_comms_init(xbee_comms_context_t* xbee_comms_ptr, TX_QUEUE* rx_queue, T
     return tx_status;
 }
 
-void xbee_comms_entry(ULONG input)
+static void xbee_comms_entry(ULONG input)
 {
     xbee_comms_context_t* xbee_comms_ptr = (xbee_comms_context_t*) input;
-    pdu_t* rx;
+    pdu_t* rd_pdu_ptr ;
 
     while(1)
     {
         int status;
         //@Todo Payload data queue receive
-        status = tx_queue_receive(xbee_comms_ptr->rx_queue, &rx, TX_WAIT_FOREVER);
+        status = tx_queue_receive(xbee_comms_ptr->rx_queue, &rd_pdu_ptr , TX_WAIT_FOREVER);
         if(status!= TX_SUCCESS)
         {
             return;
@@ -133,10 +135,10 @@ void xbee_comms_entry(ULONG input)
         //Create envelope
         wpan_envelope_t transmit_envelope;
 
-        wpan_envelope_create(&transmit_envelope, &xbee_dev.wpan_dev, WPAN_IEEE_ADDR_BROADCAST, WPAN_NET_ADDR_UNDEFINED); 
+        wpan_envelope_create(&transmit_envelope, &xbee_comms_ptr->xbee_dev.wpan_dev, WPAN_IEEE_ADDR_BROADCAST, WPAN_NET_ADDR_UNDEFINED); 
 
-        transmit_envelope.payload = rx;
-        transmit_envelope.length = sizeof(*rx);
+        transmit_envelope.payload = rd_pdu_ptr ;
+        transmit_envelope.length = sizeof(*rd_pdu_ptr );
         //Use an explicit transmit frame (type 0x11) with WPAN_ENDPOINT_DIGI_DATA as the source and
         /// destination endpoint, DIGI_CLUST_SERIAL as the cluster ID and WPAN_PROFILE_DIGI as the profile ID.
         //Send frame
@@ -147,7 +149,7 @@ void xbee_comms_entry(ULONG input)
         //wpan_tick(&xbee_dev.wpan_dev); // @todo Do we need this?
         // @todo Currently based on blocking UART function (see xbee_serial_stm32). Should it be non-blocking interrupt based? 
         // This step may be skipped if not counting stats (initially).
-        xbee_dev_tick(&xbee_dev);
+        xbee_dev_tick(&xbee_comms_ptr->xbee_dev);
     }
 
     return;
@@ -155,16 +157,16 @@ void xbee_comms_entry(ULONG input)
 
 /* Function for handling Received Transmit Status Frame */
 
-int transmit_status_handler( xbee_dev_t *xbee,
+static int transmit_status_handler( xbee_dev_t *xbee,
    const void FAR *frame, uint16_t length, void FAR *context){
 
     const xbee_frame_transmit_status_t FAR *payload = frame;
 
-    XBEE_UNUSED_PARAMETER(xbee_dev);
+    XBEE_UNUSED_PARAMETER(xbee);
     XBEE_UNUSED_PARAMETER(length);
     XBEE_UNUSED_PARAMETER(context);
 
     /* @todo If success - implement rx/tx stats counter */
 
-    return;
+    return 0;
 }
